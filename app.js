230caf8a16c766c44fcea82b638bc4a6dc697cc0
Ryan Parker <ryansparker@gmail.com>
2020-07-26 22:26:42 -0400

    dotenv file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV || "development"}`})
+console.log(`Running in ${process.env.NODE_ENV || "development"}`)
+
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
-
+console.log(process.env.NODE_ENV)
 const savedRoute = require('./routes/saved')
 
 app.use('/saved', savedRoute)
